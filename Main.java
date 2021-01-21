e996f85cae902f3c97bd7567e4a2ebe838db2494
javim7 <61723252+javim7@users.noreply.github.com>
2021-01-21 16:53:24 -0600

    Completando la clase mian
    ya se llamaron todos los metodos en sus respectivas opciones

--- Main.java
+++ Main.java
@@ -73,3 +73,87 @@ class Main {
           radio.incrementar();
 
           break;
+        case 4:
+          // Guardar emisora en unos de los 12 botones
+          System.out.println("¿En que botón desea guardar la radio?");
+          int boton; //creando variable de los botones disponiles
+
+          //while para asegurar que el boton sea un numero y caracter valido
+          while(true){
+            try{
+                System.out.print("Boton: ");
+                boton = scan.nextInt();
+                System.out.println("\n");
+                //(Programación defensiva)
+                //Protección por si el usuario elige un número menor a uno o mayor a dos, seguirá pidiendo la opción. 
+                if(boton > 12 || boton < 1){
+                    System.out.println("Solo se aceptan numeros del 1 al 12, intente de nuevo!");
+                }
+                //Si el usuario ingresa los datos correctos terminará el ciclo while
+                else{break;}
+            }
+            //Si el usuario ingresa una letra regresará un mensaje de error. 
+            catch(Exception o){
+                scan.nextLine();
+                System.out.println("Caracter invalido! Intenta de nuevo..");
+            }
+          }
+
+          radio.asignar(boton); //llamando el metodo con el respectivo boton
+
+          break;
+        case 5:
+          // Seleccionar una emisora guardada
+          System.out.println("¿Que botón de la radio desea escuchar?");
+          int b; //creando la variable del boton a Seleccionar
+
+          while(true){
+            try{
+                System.out.print("Boton: ");
+                b = scan.nextInt();
+                System.out.println("\n");
+                //(Programación defensiva)
+                //Protección por si el usuario elige un número menor a uno o mayor a dos, seguirá pidiendo la opción. 
+                if(b > 12 || b < 1){
+                    System.out.println("Solo se aceptan numeros del 1 al 12, intente de nuevo!");
+                }
+                //Si el usuario ingresa los datos correctos terminará el ciclo while
+                else{break;}
+            }
+            //Si el usuario ingresa una letra regresará un mensaje de error. 
+            catch(Exception o){
+                scan.nextLine();
+                System.out.println("Caracter invalido! Intenta de nuevo..");
+            }
+          }
+
+
+          radio.emisora(b);//llamando el metodo de seleccionar boton
+
+          break;
+        case 6:
+          // Apagar la radio
+          radio.apagar();
+
+          break;
+        case 7:
+          // Salir
+          System.out.println("\nGracias por utilizar la simulacion, vuelva pronto!");
+
+          //saliendo de la radio
+          System.exit(0);
+
+          break;
+        default:
+          // code block
+      }
+
+
+
+
+    }
+
+  scan.close();
+
+  }
+}
