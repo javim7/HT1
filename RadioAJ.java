24c4eca1b755058e52f1674d9c6f81d6af4b332c
javim7 <61723252+javim7@users.noreply.github.com>
2021-01-21 16:55:39 -0600

    Completando todas las opciones en la clase radio y completando el programa

--- RadioAJ.java
+++ RadioAJ.java
@@ -10,7 +10,7 @@ Este programa tiene como objetivo el crear una simulación de una radio
 */
 
 //importando las clases necesarias externas
-import java.util.*;
+//import java.util.*;
 
 public class RadioAJ implements RadioGeneral { //creando la clase Radio que implementa la interfaz 
 
@@ -18,7 +18,6 @@ public class RadioAJ implements RadioGeneral { //creando la clase Radio que impl
 
 int[] amEmisoras = new int[12]; //lista para guardar los valores de las emisoras am
 double[] fmEmisoras = new double[12]; //lista  para guardas los valores de las emisoras fm
-private int boton; //boton para poder asignar las emisoras de nRadio
 private boolean encendido = false; //estado de la radio de encendido o apagado, apagado por default
 private int queFrecuencia = 0; //frecuencia am o fm, am es la default y es 0
 private int amEstacion = 530; //estacion am empieza con el valor mas bajo siempre y sube de 10 en 10
@@ -116,3 +115,127 @@ boolean asig = false;// viendo si ya se asigno un boton o no
 
   }
 
+
+  //metodo para guardar una emisiora en uno de los 12 botones
+  // crea lista para el am y el fm
+  // mostrar la lista de las emisoras si es am mostrar la lista de am
+  public boolean  asignar(int pos) {
+
+    //creando if para ver si la radio esta encendida o no
+    if(encendido==true) {
+
+      //if para ver si estamos en la frecuencia am o fm
+      if(queFrecuencia==0) { //asegurando que estamos en la frecuencia am
+
+      System.out.println("\nAsignando la emisora: " + amEstacion + " en el boton: " + pos);
+      System.out.println("Asignacion de frecuencia FM exitosa!");
+
+      //guardando la emisora seleccionada en la array
+      amEmisoras[(pos-1)] = Integer.valueOf(amEstacion);
+      //System.out.println(Arrays.toString(amEmisoras));
+
+
+      } else if(queFrecuencia==1) { //asegurando que estamos en la frecuencia fm
+
+      System.out.println("\nAsignando la emisora: " + fmEstacion + " en el boton: " + pos);
+      System.out.println("Asignacion de frecuencia FM exitosa!");
+
+      //guardando la emisora seleccionada en la array
+      fmEmisoras[(pos-1)] = Double.valueOf(fmEstacion);
+      //System.out.println(Arrays.toString(fmEmisoras));
+
+      asig = true; //retornando true ya que se pudo asignar la emisora
+    }
+
+    } else {
+
+      //mostrando mensaje de error ya que la radio no esta encendida
+      System.out.println("Debe encender la radio para poder utilizar esta funcion!");
+      asig=false;
+    }
+
+    return asig;
+
+  }
+
+  //seleccionar una de las emisoras guardadas 
+  public boolean emisora(int pos) {
+
+    boolean selec = false;
+
+    //creando if para ver si la radio esta encendida o no
+    if(encendido==true) {
+
+      //if para ver si esta en am o fm
+      if(queFrecuencia==0) { //viendo si esta en am
+
+      //buscando el valor de la emisora con la posicion
+      int emi = amEmisoras[(pos-1)];
+
+      //if para dar un mensaje en caso de que no haya estacion guardada con ese boton
+      if(emi==0) {
+        System.out.println("\nUsted no ha asignado ninguna emisora en este boton, use la opcion 4 para hacerlo.");
+      } else{
+
+        System.out.println("\nUsted ha seleccionado el boton: " + pos + ", ahora esta escuchando la emisora: " + emi);
+      //System.out.println(Arrays.toString(amEmisoras));
+
+      }
+    
+
+      } else if(queFrecuencia==1) { //viendo si esta en fm
+
+      //buscando el valor de la emisora con la posicion
+      double emi2 = fmEmisoras[(pos-1)];
+
+      //if para dar un mensaje en caso de que no haya estacion guardada con ese boton
+      if(emi2==0.0) {
+        System.out.println("\nUsted no ha asignado ninguna emisora en este boton, use la opcion 4 para hacerlo.");
+      } else{
+
+        System.out.println("\nUsted ha seleccionado el boton: " + pos + ", ahora esta escuchando la emisora: " + emi2);
+      //System.out.println(Arrays.toString(amEmisoras));
+
+      }
+
+      }
+
+      selec = true;
+
+
+    } else if(encendido == false) { //viendo si la radio esta apagada
+
+      //mostrando mensaje de error ya que la radio no esta encendida
+      System.out.println("Debe encender la radio para poder utilizar esta funcion!");
+      selec = false;
+    }
+
+    return selec;
+
+  }
+
+  //cambiar entre fm y am
+  public void frecuencia() {
+
+    //creando if para ver si la radio esta encendida o no
+    if(encendido==true) {
+
+      if(queFrecuencia==0) { //significa que la frecuencia esta en am
+        queFrecuencia = 1; //cambiando el valor a 1 para que sea fm
+        System.out.println("Frecuencia actual: AM\nCambiando frecuencia...\nFrecuencia seleccionada: FM");
+      } else if(queFrecuencia==1) { //significa que la frecuencia es fm
+      queFrecuencia=0;//cambiando la frecuencia a am
+      System.out.println("Frecuencia actual: FM\nCambiando frecuencia...\nFrecuencia seleccionada: AM");
+
+      }
+
+    } else {
+
+      //mostrando mensaje de error ya que la radio no esta encendida
+      System.out.println("Debe encender la radio para poder utilizar esta funcion!");
+
+    }
+
+  }
+  
+}
